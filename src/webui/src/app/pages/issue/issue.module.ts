9b1c48a1a0a37127a418d00404a664248190eb44
AbdulkerimFettahoglu <abdulkerim.fettahoglu@gmail.com>
2020-02-22 14:21:26 +0300

    "project", "issue", and "dashboard" modules and their components were created with default Angular configurations.

---
+++ src/webui/src/app/pages/issue/issue.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IssueComponent } from './issue.component';
+
+
+
+@NgModule({
+  declarations: [IssueComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class IssueModule { }
