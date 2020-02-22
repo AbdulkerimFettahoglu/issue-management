d7ba308e9814700a40fd4672c86d5f77d795b9b2
AbdulkerimFettahoglu <abdulkerim.fettahoglu@gmail.com>
2020-02-22 19:43:27 +0300

    Routing elements was added to modules. And Angular project worked successfully.

--- src/webui/src/app/app-routing.module.ts
+++ src/webui/src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
+      {path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule)},
+      {path: 'issue', loadChildren: () => import('./pages/issue/issue.module').then(n => n.IssueModule)},
+      {path: 'project', loadChildren: () => import('./pages/project/project.module').then(i => i.ProjectModule)}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/webui/src/app/pages/issue/issue.module.ts
+++ src/webui/src/app/pages/issue/issue.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IssueComponent } from './issue.component';
 
+import { IssueRoutingModule } from './issue-routing.module';
+import { IssueComponent } from './issue.component';
 
 
 @NgModule({
   declarations: [IssueComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IssueRoutingModule
   ]
 })
 export class IssueModule { }
