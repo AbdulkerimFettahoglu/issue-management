96e12b8f4f1d4aa4a407a5082f6519e014e985c2
AbdulkerimFettahoglu <abdulkerim.fettahoglu@gmail.com>
2020-03-31 21:57:14 +0300

    CORS problem was fixed after add "WebMvcConfigurer".

--- src/main/java/com/kerimfettahoglu/issuemanagementkerim/IssueManagementKerimApplication.java
+++ src/main/java/com/kerimfettahoglu/issuemanagementkerim/IssueManagementKerimApplication.java
@@ -4,12 +4,26 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class IssueManagementKerimApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IssueManagementKerimApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 	@Bean
